2891869424cf2bd5ccc4de6bc2301dfff9cc4f72
UtkuKaganYavuzdogannn <Utku_K_Y@outlook.com>
2026-01-21 16:42:12 +0300

    relayer added.

--- order-service/src/main/java/com/turkcell/order_service/messaging/outbox/OutboxRepository.java
+++ order-service/src/main/java/com/turkcell/order_service/messaging/outbox/OutboxRepository.java
@@ -1,4 +1,12 @@
 package com.turkcell.order_service.messaging.outbox;
 
-public class OutboxRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface OutboxRepository extends JpaRepository<OutboxMessage, UUID> {
+
+    List<OutboxMessage> findByStatusOrderByCreatedAtAsc(OutboxStatus status);
+
 }
