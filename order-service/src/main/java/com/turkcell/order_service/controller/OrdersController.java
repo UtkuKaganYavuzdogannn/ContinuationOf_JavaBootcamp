422db22b30c440c5b99cebd6076f4b3a0c250d4e
UtkuKaganYavuzdogannn <Utku_K_Y@outlook.com>
2026-01-22 19:22:12 +0300

    Controller added.

--- order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
+++ order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
@@ -1,11 +1,47 @@
 package com.turkcell.order_service.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.turkcell.order_service.messaging.outbox.OutboxMessage;
+import com.turkcell.order_service.messaging.outbox.OutboxRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrdersController {
+    private final OutboxRepository outboxRepository;
+    private final ObjectMapper objectMapper;
+
+    public OrdersController(OutboxRepository outboxRepository,ObjectMapper objectMapper){
+        this.objectMapper=objectMapper;
+        this.outboxRepository=outboxRepository;
+    }
+
+    @PostMapping
+    public String createOrder(@RequestBody CreateOrderDto dto) throws JsonProcessingException{
+            OrderCreatedEvent event = new OrderCreatedEvent(dto.productId());
+
+        OutboxMessage outboxMessage = new OutboxMessage();
+        outboxMessage.setAggregateId(UUID.randomUUID()); // normalde db'de oluşan order'ın idsi
+        outboxMessage.setAggregateType("Order");
+        outboxMessage.setEventId(UUID.randomUUID());
+        outboxMessage.setEventType("OrderCreatedEvent");
+        outboxMessage.setPayloadJson(objectMapper.writeValueAsString(event));
+        outboxRepository.save(outboxMessage);
+
+        return dto.productId();
+    }
+
+    record CreateOrderDto(String productId) {
+    }
+
+    public record OrderCreatedEvent(String productId) {
+    }
+    }
 
 
-}
